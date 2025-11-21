27003512844b5cd6e2601e25dbb63012666995b6
fred5551 <fredericbitsindou246@gmail.com>
2025-11-21 17:13:57 +0100

    MonstersWeb2025

--- src/main/kotlin/org/ldv/monstersweb/MonstersWebApplication.kt
+++ src/main/kotlin/org/ldv/monstersweb/MonstersWebApplication.kt
@@ -1,9 +1,15 @@
 package org.ldv.monstersweb
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@ComponentScan(basePackages = ["org.ldv.monstersweb", "org.ldv.service"])
+@EnableJpaRepositories(basePackages = ["org.ldv.model.dao"])
+@EntityScan(basePackages = ["org.ldv.model.entity"])
 class MonstersWebApplication
 
 fun main(args: Array<String>) {
